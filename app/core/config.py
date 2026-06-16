8b2ddf81776783588e70e48d79c6fa06f7ddb702
ILYA NIKOLAENKO <dad.siberian@gmail.com>
2026-06-16 10:53:38 +0300

    feat: добавляет настройки для подключения к базе данных PostgreSQL

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -8,6 +9,23 @@ class Settings(BaseSettings):
         extra="ignore",
     )
     debug: bool = True
+
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str = "db"
+    postgres_port: int = 5432
+
+    @property
+    def database_url(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            path=self.postgres_db,
+        )
 
 
 settings = Settings()
